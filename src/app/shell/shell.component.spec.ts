8aeeb46d974044b82a6b24f7c91c6db5712c0eac
aaron-bond <aaron-bond@users.noreply.github.com>
2018-09-17 00:37:41 +0100

    Update shell.component.spec.ts

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { XboxAPI } from '@app/api/xbox.api';
 import { ShellComponent } from './shell.component';
 
 describe('ShellComponent', () => {
@@ -10,6 +11,7 @@ describe('ShellComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
       imports:[FormsModule],
+      providers: [XboxAPI],
 			declarations: [ShellComponent],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA]
 		}).compileComponents();
