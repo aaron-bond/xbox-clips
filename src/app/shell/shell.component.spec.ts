fe2a02cb1056ff46bea2780968498f034696589f
aaron-bond <aaron-bond@users.noreply.github.com>
2018-09-17 00:28:47 +0100

    Update shell.component.spec.ts

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ShellComponent } from './shell.component';
 
 describe('ShellComponent', () => {
@@ -8,6 +9,7 @@ describe('ShellComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+      imports:[FormsModule],
 			declarations: [ShellComponent],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA]
 		}).compileComponents();
